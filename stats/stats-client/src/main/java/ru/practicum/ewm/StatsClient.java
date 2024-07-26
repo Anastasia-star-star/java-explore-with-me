08cb2c63b0da5f09f9a0113ad7fcb9f14973e9d7
Anastasiia <aiarkhipova@yandex.ru>
2024-07-26 15:11:15 +0300

    fix: 19

--- stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.ewm.exceptions.ClientException;
 
@@ -25,6 +26,7 @@ public class StatsClient {
 
     private final Gson gson;
 
+    @Autowired
     public StatsClient(String serverUrl, Gson gson) {
         this.serverUrl = serverUrl;
         this.gson = gson;
