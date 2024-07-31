ee81da816f6a79637e4a1e07690b016a428890bf
Anastasiia <aiarkhipova@yandex.ru>
2024-07-31 17:08:27 +0300

    fix: 50

--- ewm-service/src/main/java/ru/practicum/ewm/compilation/controller/CompilationPublicController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/compilation/controller/CompilationPublicController.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.PositiveOrZero;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,19 +20,18 @@ public class CompilationPublicController {
     private final CompilationPublicService publicService;
 
     @GetMapping
-    public ResponseEntity<List<CompilationDto>> getAllCompilations(
+    public List<CompilationDto> getAllCompilations(
             @RequestParam(required = false) Boolean pinned,
             @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
             @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
         log.info("get all compilations, pinned = {}, from = {}, size = {}.", pinned, from, size);
-        return ResponseEntity.ok().body(publicService.getAllCompilations(pinned, from, size));
+        return publicService.getAllCompilations(pinned, from, size);
     }
 
     @GetMapping("/{compId}")
-    public ResponseEntity<CompilationDto> getCompilationById(@PathVariable Long compId) {
-        CompilationDto compilationDto = publicService.getCompilationById(compId);
-        log.info("Получена подборка событий с id = {}.", compId);
-        return ResponseEntity.ok(compilationDto);
+    public CompilationDto getCompilationById(@PathVariable Long compId) {
+        log.info("Get compilation by id {}", compId);
+        return publicService.getCompilationById(compId);
     }
 
 }
