bb9175502a018344d57e4c6334fcf8dbd4c8200e
Anastasiia <aiarkhipova@yandex.ru>
2024-08-06 15:36:08 +0300

    fix: 38k

--- ewm-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -16,37 +16,41 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByInitiatorId(Long userId, Pageable page);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE ((:users is null or e.initiator.id IN :users) " +
-            "AND (:states is null or e.state IN :states) " +
-            "AND (:categories is null or e.category.id IN :categories) " +
-            "AND (e.eventDate BETWEEN :rangeStart and :rangeEnd)) ")
-    List<Event> getAllEventsByAdmin(@Param("users") List<Long> users, @Param("states") List<StateEvent> states,
-            @Param("categories") List<Long> categories,
-            @Param("rangeStart") LocalDateTime rangeStart,
-            @Param("rangeEnd") LocalDateTime rangeEnd,
-            Pageable page);
+            "WHERE (:users IS NULL OR e.initiator.id IN :users) " +
+            "AND (:states IS NULL OR e.state IN :states) " +
+            "AND (:categories IS NULL OR e.category.id IN :categories) " +
+            "AND e.eventDate BETWEEN :rangeStart AND :rangeEnd")
+    List<Event> getAllEventsByAdmin(@Param("users") List<Long> users,
+                                    @Param("states") List<StateEvent> states,
+                                    @Param("categories") List<Long> categories,
+                                    @Param("rangeStart") LocalDateTime rangeStart,
+                                    @Param("rangeEnd") LocalDateTime rangeEnd,
+                                    Pageable page);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE (e.state = 'PUBLISHED') " +
+            "WHERE e.state = 'PUBLISHED' " +
             "AND (LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
             "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-            "AND ((:categories) IS NULL OR e.category.id IN :categories) " +
-            "AND ((:paid) IS NULL OR e.paid = :paid) " +
-            "AND (e.eventDate BETWEEN :rangeStart AND :rangeEnd) " +
-            "AND ((:onlyAvailable IS TRUE AND (e.participantLimit > e.confirmedRequests OR e.participantLimit = 0)) " +
-            "    OR :onlyAvailable IS FALSE) ")
-    List<Event> getAllEvents(@Param("text") String text, @Param("categories") List<Long> categories,
+            "AND (:categories IS NULL OR e.category.id IN :categories) " +
+            "AND (:paid IS NULL OR e.paid = :paid) " +
+            "AND e.eventDate BETWEEN :rangeStart AND :rangeEnd " +
+            "AND (:onlyAvailable = FALSE OR (e.participantLimit = 0 OR e.participantLimit > e.confirmedRequests))")
+    List<Event> getAllEvents(@Param("text") String text,
+                             @Param("categories") List<Long> categories,
                              @Param("paid") Boolean paid,
                              @Param("rangeStart") LocalDateTime rangeStart,
                              @Param("rangeEnd") LocalDateTime rangeEnd,
                              @Param("onlyAvailable") Boolean onlyAvailable,
                              Pageable page);
 
-    @Query("SELECT e FROM Event e WHERE (e.state = 'PUBLISHED') " +
-            "AND (function('distance', e.location.lat, e.location.lon, :lat, :lon) <= :radius) " +
-            "AND (e.eventDate BETWEEN :rangeStart AND :rangeEnd) ")
-    List<Event> getAllEventsByLocation(@Param("lat") Float lat, @Param("lon") Float lon, @Param("radius") Float radius,
-            @Param("rangeStart") LocalDateTime rangeStart,
-            @Param("rangeEnd") LocalDateTime rangeEnd,
-            Pageable page);
+    @Query("SELECT e FROM Event e " +
+            "WHERE e.state = 'PUBLISHED' " +
+            "AND function('distance', e.location.lat, e.location.lon, :lat, :lon) <= :radius " +
+            "AND e.eventDate BETWEEN :rangeStart AND :rangeEnd")
+    List<Event> getAllEventsByLocation(@Param("lat") Float lat,
+                                       @Param("lon") Float lon,
+                                       @Param("radius") Float radius,
+                                       @Param("rangeStart") LocalDateTime rangeStart,
+                                       @Param("rangeEnd") LocalDateTime rangeEnd,
+                                       Pageable page);
 }
